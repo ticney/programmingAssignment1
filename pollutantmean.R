180d644338607678943821c6beb26a089fb2fd53
ticney <geochampetier@gmail.com>
2020-09-25 18:40:51 +0200

    small fixes

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,11 +1,11 @@
-pollutantmeanMerge <- function(directory, pollutant, id= 1:332){
+pollutantmean <- function(directory, pollutant, id= 1:332){
 
-  csvId <- list.files(directory, pattern = "*.csv")
+  csvId <- list.files(paste(getwd(),"/",directory, sep = ""), pattern = "*.csv")
   
   mytempdata <- data.frame("Date" = 0, "sulfate"=0, "nitrate"= 0,"ID"=0)  
   
   for (val in csvId[id]) {
-    mytempdata <- rbind(mytempdata,read.csv(val))
+    mytempdata <- rbind(mytempdata,read.csv(paste(directory,"/",val, sep = "")))
   }
   
   dataClean<-mytempdata[complete.cases(mytempdata[,pollutant]),]
