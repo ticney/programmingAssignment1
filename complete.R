f637a216ddfec2b9531391a9679737d0ebd6ee16
ticney <geochampetier@gmail.com>
2020-09-25 18:30:14 +0200

    3rd function added

--- complete.R
+++ complete.R
@@ -1,7 +1,5 @@
 complete <- function(directory, id = 1:332){
   
-  test<- paste(getwd(),"/",directory, sep = "")
-  
   csvId <- list.files(paste(getwd(),"/",directory, sep = ""), pattern = "*.csv")
   
   mytempdata <- data.frame(id = character(), nobs = character())
